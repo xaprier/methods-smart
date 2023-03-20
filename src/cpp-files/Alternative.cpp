7903cbfcd7925882cea83853744d9caea0e1d947
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-20 21:54:03 +0300

    Added cpp-files for solution

--- src/cpp-files/Alternative.cpp
+++ src/cpp-files/Alternative.cpp
@@ -2,4 +2,15 @@
 // Created by xaprier on 3/20/23.
 //
 
-#include "Alternative.h"
+#include "../header-files/Alternative.h"
+
+Alternative::Alternative(const std::string &alternativeName, int attributeCount, const Attribute *referenceAttribute) : alternativeName(alternativeName) {
+    float value;
+    for (int i = 0; i < attributeCount; ++i) {
+        std::cout << "Enter the value of " << alternativeName << "'s " << referenceAttribute[i].attributeName << " attribute value: ";
+        std::cin >> value;
+        Attribute attribute(value, referenceAttribute[i]);
+    }
+}
+
+Alternative::Alternative() { }
--- src/cpp-files/NormalizedWeight.cpp
+++ src/cpp-files/NormalizedWeight.cpp
@@ -2,4 +2,10 @@
 // Created by xaprier on 3/20/23.
 //
 
-#include "NormalizedWeight.h"
+#include "../header-files/NormalizedWeight.h"
+
+NormalizedWeight::NormalizedWeight(int rank) : rank(rank) { }
+
+float NormalizedWeight::operator=(const NormalizedWeight &other) {
+    return 0;
+}
