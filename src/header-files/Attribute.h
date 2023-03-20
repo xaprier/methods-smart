1b47caf193599d50f4ee2fc81e0809f81b61e5e6
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-20 21:52:13 +0300

    Update Solution Template

---
+++ src/header-files/Attribute.h
@@ -0,0 +1,14 @@
+//
+// Created by xaprier on 3/20/23.
+//
+
+#ifndef METHODS_SMART_ATTRIBUTE_H
+#define METHODS_SMART_ATTRIBUTE_H
+
+
+class Attribute {
+
+};
+
+
+#endif //METHODS_SMART_ATTRIBUTE_H
---
+++ src/header-files/EffectiveWeight.h
@@ -0,0 +1,14 @@
+//
+// Created by xaprier on 3/20/23.
+//
+
+#ifndef METHODS_SMART_EFFECTIVEWEIGHT_H
+#define METHODS_SMART_EFFECTIVEWEIGHT_H
+
+
+class EffectiveWeight {
+
+};
+
+
+#endif //METHODS_SMART_EFFECTIVEWEIGHT_H
