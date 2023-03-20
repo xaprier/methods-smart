756c254bcb8d2e6f27dda56fb6d25f65d03fe14c
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-20 21:54:03 +0300

    Added cpp-files for solution

--- src/cpp-files/Attribute.cpp
+++ src/cpp-files/Attribute.cpp
@@ -2,4 +2,28 @@
 // Created by xaprier on 3/20/23.
 //
 
-#include "Attribute.h"
+#include "../header-files/Attribute.h"
+
+Attribute::Attribute(float value, const Attribute &other) : min(other.min), max(other.max), rank(other.rank), type(other.type), attributeName(other.attributeName) {
+    this->value = value;
+
+    auto normalizedWeight = NormalizedWeight(rank);
+    auto attributeRate = AttributeRate(min, max);
+
+    nWeight = normalizedWeight.operator=(normalizedWeight);
+    rate = attributeRate.operator=(attributeRate);
+
+    auto eWeight = EffectiveWeight(value, rate, type);
+
+    effectiveWeight = eWeight.operator=(eWeight);
+}
+
+Attribute::Attribute(int min, int max, int rank, bool type, std::string attributeName) : min(min), max(max), rank(rank), type(type), attributeName(attributeName) { }
+
+Attribute::Attribute() {
+    char c;
+    std::cout << "Enter the attribute's name, minimum value, maximum value, rank in your decision, type(- +):";
+    std::cin >> attributeName >> min >> max >> rank >> c;
+    type = c == '+';
+}
+
--- src/cpp-files/SmartMethod.cpp
+++ src/cpp-files/SmartMethod.cpp
@@ -2,4 +2,22 @@
 // Created by xaprier on 3/20/23.
 //
 
-#include "SmartMethod.h"
+#include "../header-files/SmartMethod.h"
+#include <iostream>
+SmartMethod::SmartMethod() {
+    std::cout << "How many attribute there will be: ";
+    int attributeCount;
+    std::cin >> attributeCount;
+    attributeBases = new Attribute[attributeCount];
+
+    std::cout << "How many alternatives there will be: ";
+    int alternativeCount;
+    std::cin >> alternativeCount;
+    alternatives = new Alternative[alternativeCount];
+    for (int i = 0; i < alternativeCount; ++i) {
+        std::cout << "Enter the name of the " << i + 1 << ". alternative: ";
+        std::string alternativeName;
+        std::cin >> alternativeName;
+        alternatives[i] = Alternative(alternativeName, attributeCount, &*attributeBases);
+    }
+}
