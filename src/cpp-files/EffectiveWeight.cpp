c4ab2a7468a92e410a737b7ba0c49da98affb542
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-22 21:40:21 +0300

    Wrong commit

--- src/cpp-files/EffectiveWeight.cpp
+++ src/cpp-files/EffectiveWeight.cpp
@@ -3,13 +3,19 @@
 //
 
 #include "../header-files/EffectiveWeight.h"
+#include <cmath>
 
 EffectiveWeight::EffectiveWeight(float value, float *rate, bool type) : value(value), rate(rate), type(type) {
 
 }
 
-
 float EffectiveWeight::operator=(const EffectiveWeight &other) {
     // calculate effective weight of alternative's attribute and return
-    return 0;
+    float result = 0;
+    if (type) {
+        result = 4 + log2(((value - rate[0]) / (rate[1] - rate[0])) * 64);
+    } else {
+        result = 10 - log2(((value - rate[0]) / (rate[1] - rate[0])) * 64);
+    }
+    return result;
 }
