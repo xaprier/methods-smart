9627dd8f6735afa56b1567ec50b141fb737ad8f8
Seymen Kalkan <seymenkalkan@gmail.com>
2023-03-22 21:30:49 +0300

    Calculated NormalizedWeight for each attribute

--- src/cpp-files/NormalizedWeight.cpp
+++ src/cpp-files/NormalizedWeight.cpp
@@ -3,12 +3,16 @@
 //
 
 #include "../header-files/NormalizedWeight.h"
-
+#include <cmath>
 NormalizedWeight::NormalizedWeight(int rank, const std::vector<int> *ranks) : rank(rank) {
     this->ranks = ranks;
 }
 
 float NormalizedWeight::operator=(const NormalizedWeight &other) {
     // calculate the normalized weight for attribute and return
-    return 0;
+    float sum = 0;
+    for (int i = 0; i < ranks->size(); ++i) {
+        sum += pow(sqrt(2), ranks->at(i));
+    }
+    return pow(sqrt(2), rank) / sum;
 }
